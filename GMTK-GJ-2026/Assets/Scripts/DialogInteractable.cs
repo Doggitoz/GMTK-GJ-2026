1fb0cb5af1b372f9c709a8391ff48e9cdddf9be6
Coleton Wheeler <cpw2965@utulsa.edu>
2026-07-24 18:24:13 -0500

    Initial setup

---
+++ GMTK-GJ-2026/Assets/Scripts/DialogInteractable.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class DialogInteractable : MonoBehaviour, IInteractable
+{
+    public void OnInteractorDown(Transform interactor)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnInteractorHover()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnInteractorLeave()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnInteractorStay(Transform interactor)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnInteractorUp(Transform interactor)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
