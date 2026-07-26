28c982dd4cbd4160de91a46c180d1bb840a86fc8
BrennanCrowder <brennancrowder19@gmail.com>
2026-07-26 04:26:44 -0500

    Update Manager

---
+++ GMTK-GJ-2026/Assets/Scripts/GameTimer.cs
@@ -0,0 +1,25 @@
+using Clock;
+using TMPro;
+using UnityEngine;
+
+public class GameTimer : MonoBehaviour
+{
+    [SerializeField]
+    TMP_Text _timerText;
+
+    private void Update()
+    {
+        UpdateTimer(TimeManager.Instance.Minutes, TimeManager.Instance.Seconds);
+    }
+
+    public void UpdateTimer(int minutes,int seconds)
+    {
+        int mtens = (minutes / 10) % 10;
+        int mones = (minutes % 10);
+
+        int stens = (seconds / 10) % 10;
+        int sones = (seconds % 10);
+
+        _timerText.text = $"{mtens} {mones}  {stens} {sones}";
+    }
+}
