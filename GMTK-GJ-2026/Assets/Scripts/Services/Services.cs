f5a9ccfb37fbf926cbcdfca0caec32c99d888b4e
Coleton Wheeler <dev.coleton@gmail.com>
2026-08-06 02:44:16 -0500

    Update Services.cs

--- GMTK-GJ-2026/Assets/Scripts/Services/Services.cs
+++ GMTK-GJ-2026/Assets/Scripts/Services/Services.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 public static class Services
 {
     public static GameServices Game => GameServices.Instance;
-    public static SaveService Save => GameServices.Instance.Save;
-    public static InventoryService Inventory => GameServices.Instance.Inventory;
-    public static ProgressService Progress => GameServices.Instance.Progress;
-    public static CurrencyService Currency => GameServices.Instance.Currency;
+    public static SaveService Save => Game.Save;
+    public static InventoryService Inventory => Game.Inventory;
+    public static ProgressService Progress => Game.Progress;
+    public static CurrencyService Currency => Game.Currency;
 }
